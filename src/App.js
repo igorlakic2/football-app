2ef0e7fa318d0f1da4a78dc3b1e6054000c5a0f0
unknown <igor.lakic2@gmail.com>
2022-09-09 20:00:02 +0200

    webpack react configuration

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+// import Home from "./pages/Home";
+// import Products from "./pages/Products/Products";
+// import Header from "./components/Header/Header";
+// import SingleProduct from "./pages/Products/SingleProduct";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Football APP.JS</h1>
+      {/* <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/o-nama" element={<About />} />
+          <Route path="/web-izlog" element={<Products />} />
+          <Route path="/kontakt" element={<Contact />} />
+          <Route path="/web-izlog/:name/:id" element={<SingleProduct />} />
+        </Routes>
+      </BrowserRouter> */}
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import "./styles/main.scss";
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+import App from "./App";
 
-console.log("test webpack");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
