95b75b3d0ddf6cb6a6866d2564a66ab7ff78012c
unknown <igor.lakic2@gmail.com>
2022-09-20 21:15:59 +0200

    leagues page

--- src/pages/Leagues.js
+++ src/pages/Leagues.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CountriesAutocomplete from "../components/FormsUI/CountriesAutocomplete/CountriesAutocomplete";
+import SimpleModal from "../components/Modal/Modal";
+import { getCountries, getLeagues } from "../util/getFunctions";
 
 const Leagues = () => {
-  return <div>Leagues</div>;
+  const [countries, setCountries] = useState([]);
+  const [load, setLoad] = useState(false);
+  const [countryId, setCountryId] = useState("");
+  const [leagues, setLeagues] = useState([]);
+
+  useEffect(() => {
+    getCountries(setCountries);
+  }, []);
+
+  useEffect(() => {
+    if (countryId !== "") getLeagues(countryId, setLeagues, setLoad);
+  }, [countryId]);
+
+  return (
+    <div style={{ padding: 20 }}>
+      <CountriesAutocomplete
+        countries={countries}
+        onChange={(event) => setCountryId(event?.country_id)}
+      />
+      {load && <SimpleModal />}
+    </div>
+  );
 };
 
 export default Leagues;
